5a9bdb329d5ed1bb6bb8cf276e8a2c9a35d9ebcf
Michael Gaudet <Michael.G224@gmail.com>
2015-12-03 10:25:33 +0000

    hopefully fixxed this dum DB mistake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202105141) do
+ActiveRecord::Schema.define(version: 20151203102110) do
 
   create_table "contacts", force: true do |t|
   end
